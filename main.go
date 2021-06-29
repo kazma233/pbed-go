e949f43f89b3c85dfb5ed7c5ce77bf404648ba3b
kazma233 <kazma233@outlook.com>
2021-06-29 20:10:06 +0800

    feat: cli

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"flag"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"pbed/cons"
 	"pbed/xgithub"
+	"strings"
 
 	"github.com/mitchellh/go-homedir"
 )
 
-func init() {
+func InitConfig() {
 	home, err := homedir.Dir()
 	if err != nil {
 		panic(err)
@@ -39,12 +43,50 @@ func init() {
 }
 
 func main() {
-	b := xgithub.New()
+	init := flag.Bool("init", false, "init config file")
+	allDir := flag.Bool("all", false, "upload current dir: ./")
+	path := flag.String("p", "", "upload file path")
 
-	p, err := b.Upload("any file")
-	if err != nil {
-		panic(err)
+	flag.Parse()
+
+	if *init {
+		log.Println("init config")
+		InitConfig()
+
+		return
 	}
 
-	log.Println(p)
+	b := xgithub.New()
+
+	if *allDir {
+		filepath.Walk("./", func(path string, info fs.FileInfo, err error) error {
+			// ignore dir
+			if info.IsDir() {
+				return nil
+			}
+
+			// ignore hidden file
+			if strings.HasPrefix(info.Name(), ".") || strings.HasPrefix(path, ".") {
+				return nil
+			}
+
+			log.Println(path)
+
+			return nil
+		})
+
+		return
+	}
+
+	if *path != "" {
+		p, err := b.Upload(*path)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("upload url: %s", p)
+		return
+	}
+
+	log.Println("use -h to get help")
 }
